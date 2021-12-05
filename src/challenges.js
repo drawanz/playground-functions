f75f032fc316223a8f443e7a88597c0668d9a894
Leonidas <leonidas.drawanz@gmail.com>
2021-12-05 11:50:12 -0300

    primeira parte do exercicio 9

--- src/challenges.js
+++ src/challenges.js
@@ -94,9 +94,35 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(str) {
+  let arr = str.split('');
+  let newArr = [];
+
+    for (let i =0; i < arr.length; i +=1){
+        if ( arr[i] === "a"){
+            newArr.push("1");
+        }
+        else if ( arr[i] === "e"){
+            newArr.push("2");
+        }
+        else if ( arr[i] === "i"){
+            newArr.push("3");
+        }
+        else if ( arr[i] === "o"){
+            newArr.push("4");
+        }
+        else if ( arr[i] === "u"){
+            newArr.push("5");
+        }
+        else{
+            newArr.push(arr[i]);
+        } 
+        }
+  let newString = newArr.join("");
+        return newString;
 }
+
+
 function decode() {
   // seu código aqui
 }
--- src/teste.js
+++ src/teste.js
@@ -1,29 +1,33 @@
-/* Crie uma função chamada fizzBuzz que receba uma array de números e retorne uma array da seguinte forma:
+/* 
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5 */
 
-Para cada número do Array que seja divisível apenas por 3, apresente uma string "fizz";
-Para cada número do Array que seja divisível apenas por 5, apresente uma string "buzz";
-Caso o número seja divisível por 3 e 5, retorne a string "fizzBuzz";
-Caso o número não possa ser dividido por 3 nem por 5, retorne a string "bug!";
-Exemplo: caso o parâmetro seja [2, 15, 7, 9, 45], sua função deverá retornar ["bug!", "fizzBuzz", "bug!", "fizz", "fizzBuzz"]. */
-let array = [2, 15, 7, 9, 45]
-function fizzBuzz(array) {
-    let arr = [];
-    for (let i = 0; i < array.length; i +=1){
-      if ((array[i] % 3) === 0){
-          arr.push("fizz");
-      }
-      else if ((array[i] % 5) === 0){
-          arr.push("buzz");
-      }
-      else if ( (array[i] % 3) === 0 && (array[i] % 5) === 0){
-          arr.push("fizzBuzz");
-      }
-      else if ( (array[i] % 3) !== 0 && (array[i] % 5) !== 0){
-          arr.push("bug!");
-      }
-  }
-      console.log(arr);
-  }
+let str = "hi there!"
+let arr = str.split('');
+let newArr = [];
 
-
-
+for (let i =0; i < arr.length; i +=1){
+    if ( arr[i] === "a"){
+        newArr.push("1");
+    }
+    else if ( arr[i] === "e"){
+        newArr.push("2");
+    }
+    else if ( arr[i] === "i"){
+        newArr.push("3");
+    }
+    else if ( arr[i] === "o"){
+        newArr.push("4");
+    }
+    else if ( arr[i] === "u"){
+        newArr.push("5");
+    }
+    else{
+        newArr.push(arr[i]);
+    } 
+    }
+let newString = newArr.join("");
+console.log(newString);
